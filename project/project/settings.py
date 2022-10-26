f9d143279d7cd27e1354ecce3d030898500ab776
kevinchege47 <chegekevin001@gmail.com>
2022-10-26 21:32:12 +0300

    Added RetrieveAPIView and allowed all hosts

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4eb8fv7o0gf*a(u1+r07(llq2q6*4^wbi$snicopjvb6b$1m-4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
