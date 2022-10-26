ebdb3d121585eea17c58397ecedacf183e1b0ae1
kevinchege47 <chegekevin001@gmail.com>
2022-10-26 09:36:04 +0300

    Initial commit

---
+++ project/application/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Cars)
+admin.site.register(Accidents)
+admin.site.register(InsuranceCompany)
+# Register your models here.
---
+++ project/application/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class Accidents(models.Model):
+    LEVELS = (
+        ('Fatal Injury Crash','Fatal Injury Crash'),
+        ('Major Injury Crash','Major Injury Crash'),
+        ('Minor Injury Crash','Minor Injury Crash'),
+        ('Minimal Injury Crash','Minimal Injury Crash'),
+    
+    )
+    cause = models.CharField(max_length=50,null=True)
+    location = models.CharField(max_length=50,null=True)
+    description = models.CharField(max_length=50,null=True)
+    degreeseverity = models.CharField(max_length=50,null=True,choices=LEVELS)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+
+    def __int__(self):
+        return self.id
+
+class InsuranceCompany(models.Model):
+    name = models.CharField(max_length=50,null=True)
+    head_office_location = models.CharField(max_length=50,null=True)
+    phone = models.CharField(max_length=15,null=True)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+
+    def __int__(self):
+        return self.id
+class Cars(models.Model):
+
+    numberplate = models.CharField(max_length=50,null=True)
+    makemodel = models.CharField(max_length=50,null=True)
+    driverphone = models.CharField(max_length=15,null=True)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+    accidents = models.ManyToManyField(Accidents,blank=True)
+    insurancecompany = models.ManyToManyField(InsuranceCompany,blank=True)
+
+    def __str__(self):
+        return self.numberplate
+# Create your models here.
